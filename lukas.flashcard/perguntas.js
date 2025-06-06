0aff3ea77fd39637981ce42f00e5acd9196a411e
lukas70000 <lukas.cheslacki@escola.pr.gov.br>
2025-06-06 17:21:19 -0300

    Add files via upload
    projeto escola

---
+++ lukas.flashcard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Super Mario',
+    'Quem é Mario?',
+    'Super Mario é uma série de jogos eletrônicos de plataforma, criada pela Nintendo, baseada e estrelada pelo encanador fictício Mario.'
+)
+
+criaCartao(
+    'Sonic the Hedgehog',
+    'Quem é Sonic?',
+    'Sonic é um ouriço rápido, aventureiro, corajoso e extrovertido. Enfrenta todos os perigos para ajudar os outros. Mas também é preguiçoso.'
+)
+
+criaCartao(
+    'The Legend of Zelda',
+    'Quem é Link?',
+    'Link é um personagem fictício e o principal protagonista da série de jogos eletrônicos The Legend of Zelda. Um herói que parece um anão de jardim.'
+)
+
+criaCartao(
+    'Minecraft',
+    'Quem é Steve?',
+    'Steve é a principal skin do jogo Minecraft. Ele e Alex são os principais personagens padrões masculino e feminino, respectivamente.'
+)
